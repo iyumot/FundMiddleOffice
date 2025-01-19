aa4efe2c40a305465f8466f56d8289e6f79d6670
iyu <iyumot@163.com>
2025-01-19 14:47:50 +0800

    管理人页

--- FMO/Views/ManagerPage.xaml.cs
+++ FMO/Views/ManagerPage.xaml.cs
@@ -1,28 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FMO.Models;
+using FMO.Utilities;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace FMO
+namespace FMO;
+
+/// <summary>
+/// ManagerPage.xaml 的交互逻辑
+/// </summary>
+public partial class ManagerPage : UserControl
+{
+    public ManagerPage()
+    {
+        InitializeComponent();
+    }
+}
+
+public interface IDatabaseUpdater
+{
+    void Update();
+}
+
+public abstract partial class DataItem<T, TEntity> : ObservableObject
+{
+
+    public required string Label { get; set; }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsChanged))]
+    public partial T? OldValue { get; set; }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsChanged))]
+    public partial T? NewValue { get; set; }
+      
+    public bool IsChanged => NewValue is not null && !NewValue.Equals(OldValue);
+
+    public required Action<T?, TEntity> Updater { get; set; }
+
+     
+    public string? Format { get; set; }
+
+    public abstract void Update();
+}
+
+public partial class ManagerDataItem<T> : DataItem<T, Manager>, IDatabaseUpdater
+{
+
+    public override void Update()
+    {
+        using var db = new BaseDatabase();
+        var manager = db.GetCollection<Manager>().FindOne(x => x.IsMaster);
+        Updater(NewValue, manager);
+        db.GetCollection<Manager>().Update(manager);
+    }
+}
+
+
+public partial class ManagerPageViewModel : ObservableObject
 {
     /// <summary>
-    /// ManagerPage.xaml 的交互逻辑
+    /// 管理人名称
     /// </summary>
-    public partial class ManagerPage : UserControl
+    [ObservableProperty]
+    public partial ManagerDataItem<string> ManagerName { get; set; }
+
+    /// <summary>
+    /// 实控人
+    /// </summary>
+    [ObservableProperty]
+    public partial ManagerDataItem<string> ArtificialPerson { get; set; }
+     
+
+    public string? RegisterNo { get; set; }
+
+    [ObservableProperty]
+    public partial ManagerDataItem<decimal> RegisterCapital { get; set; }
+
+
+    [ObservableProperty]
+    public partial bool IsReadOnly { get; set; } = true;
+
+
+    public ManagerPageViewModel()
     {
-        public ManagerPage()
-        {
-            InitializeComponent();
-        }
+        using var db = new BaseDatabase();
+        var manager = db.GetCollection<Manager>().FindOne(x => x.IsMaster);
+
+        ManagerName = new ManagerDataItem<string> { Label = "管理人", OldValue = manager.Name, NewValue = manager.Name, Updater = (a, b) => b.Name = a! };
+
+        ArtificialPerson = new ManagerDataItem<string> { Label = "实控人", OldValue = manager.ArtificialPerson, NewValue = manager.ArtificialPerson, Updater = (a, b) => b.ArtificialPerson = a! };
+
+        RegisterNo = manager.RegisterNo;//new ManagerDataItem<string> { Label = "实控人", OldValue = manager.RegisterNo, NewValue = manager.RegisterNo, Updater = (a, b) => b.RegisterNo = a! };
+
+
+
+        RegisterCapital = new ManagerDataItem<decimal> { Label = "注册资金", OldValue = manager.RegisterCapital, NewValue = manager.RegisterCapital, Format="{0}万元", Updater = (a, b) => b.RegisterCapital = a! };
+
+    }
+
+
+    [RelayCommand]
+    public void UpdateManagerInfo(IDatabaseUpdater dataItem)
+    {
+        dataItem.Update();
     }
 }
