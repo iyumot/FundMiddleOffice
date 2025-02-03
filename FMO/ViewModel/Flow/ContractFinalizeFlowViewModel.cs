c2536fda36636673bac5ff271fcd28bdd350cdda
iyumot <iyumot@163.com>
2025-02-03 19:31:30 +0800

    增加contractflow 基类

--- FMO/ViewModel/Flow/ContractFinalizeFlowViewModel.cs
+++ FMO/ViewModel/Flow/ContractFinalizeFlowViewModel.cs
@@ -10,53 +10,27 @@ using System.Windows;
 
 namespace FMO;
 
-public partial class ContractFinalizeFlowViewModel : FlowViewModel, IElementChangable
-{
-   // private const string SingleShareName = "单一份额";
-
-    /// <summary>
-    /// 定稿合同
-    /// </summary>
-    [ObservableProperty]
-    public partial FileInfo? Contract { get; set; }
+public partial class ContractFinalizeFlowViewModel : ContractRelatedFlowViewModel, IElementChangable
+{ 
 
 
     [ObservableProperty]
     public partial FileInfo? RiskDisclosureDocument { get; set; }
 
-
-    /// <summary>
-    /// 募集账户函
-    /// </summary>
-    [ObservableProperty]
-    public partial FileInfo? CollectionAccount { get; set; }
-
-
-    /// <summary>
-    /// 托管账户函
-    /// </summary>
-    [ObservableProperty]
-    public partial FileInfo? CustodyAccount { get; set; }
-
-
-
-    [ObservableProperty]
-    public partial ObservableCollection<string> Shares { get; set; }
-
-
-    [ObservableProperty]
-    public partial bool IsDividingShare { get; set; }
+     
 
     /// <summary>
     /// 如何删除后，留下的
     /// </summary>
     private string? RemainShare { get; set; }
 
-
+    /// <summary>
+    /// 份额类型有变动
+    /// </summary>
     private bool _shareChanged;
 
     [SetsRequiredMembers]
-    public ContractFinalizeFlowViewModel(ContractFinalizeFlow flow, Mutable<ShareClass[]>? shareClass) : base(flow)
+    public ContractFinalizeFlowViewModel(ContractFinalizeFlow flow, Mutable<ShareClass[]>? shareClass) : base(flow, shareClass)
     {
         if (!string.IsNullOrWhiteSpace(flow.ContractFile?.Path))
             Contract = new FileInfo(flow.ContractFile.Path);
@@ -81,129 +55,8 @@ public partial class ContractFinalizeFlowViewModel : FlowViewModel, IElementChan
 
 
 
+     
 
-    partial void OnCollectionAccountChanged(FileInfo? oldValue, FileInfo? newValue)
-    {
-        if (!Initialized) return;
-
-        if (newValue?.Exists ?? false)
-        {
-
-            SaveFile<ContractFinalizeFlow>(newValue, "Accounts", x => x.CollectionAccountFile, x => x.CollectionAccountFile = new FundFileInfo { Name = "募集账户函" });
-
-        }
-
-    }
-
-
-    [RelayCommand]
-    public void DivideShares()
-    {
-        IsDividingShare = true;
-        _shareChanged = true;
-
-        ///最大5类
-        if (Shares.Count > 5) return;
-
-        if (Shares.Count == 1)
-            Shares[0] = "A";
-        Shares.Add(((char)('A' + Shares.Count)).ToString());
-    }
-
-    [RelayCommand]
-    public void DeleteShare(string s)
-    {
-        if (s == FundElements.SingleShareKey)
-            return;
-
-        Shares.Remove(s);
-        if (Shares.Count == 1)
-        {
-            RemainShare = Shares[0];
-            Shares[0] = FundElements.SingleShareKey;
-        }
-    }
-
-    [RelayCommand]
-    public void ConfirmShares()
-    {
-        IsDividingShare = false;
-        _shareChanged = false;
-
-        using var db = new BaseDatabase();
-        var elements = db.GetCollection<FundElements>().FindOne(x => x.FundId == FundId);
-
-        //if (elements.ShareClasses is not null && elements.ShareClasses.GetValue(FlowId) is var d && d.Value is not null && d.Value.Length >= Shares.Count && (!d.Value?.Select(x => x.Name).ToArray().SequenceEqual(Shares) ?? false))
-        //{
-        //    if (MessageBoxResult.Cancel == MessageBox.Show("减少份额种类将会删除对应份额相关的要素", "", MessageBoxButton.OKCancel))
-        //    {
-        //        if (d.Value is ShareClass[] shares)
-        //            Shares = new ObservableCollection<string>(shares.Select(x => x.Name));
-        //        else
-        //            Shares = new ObservableCollection<string>([SingleShareName]);
-
-        //        return;
-        //    }
-        //}
-
-        //// 同步份额相关的要素 
-        var rem = Shares.Count == 1 ? [RemainShare ?? Shares[0]] : Shares.ToArray();
-        var old = elements.ShareClasses is null ? [] : elements.ShareClasses.GetValue(FlowId) is var dd && dd.FlowId != FlowId ? [] : dd.Value?.Select(x => x.Name).ToArray() ?? [];
-
-        var remove = old.Except(rem).ToArray();
-        var add = rem.Except(old).ToArray();
-
-        //foreach (var item in remove) 
-        //    elements.RemoveShareRelated(FlowId, item);
-
-        //foreach (var item in add)
-        //    elements.AddShareRelated(FlowId, item);
-       
- 
-        if(remove.Any() && MessageBoxResult.Cancel == HandyControl.Controls.MessageBox.Show("减少份额种类将会删除对应份额相关的要素", "", MessageBoxButton.OKCancel))
-        {
-            if (elements.ShareClasses is not null && elements.ShareClasses.GetValue(FlowId) is var d && d.Value is ShareClass[] shares)
-                Shares = new ObservableCollection<string>(shares.Select(x => x.Name));
-            else
-                Shares = new ObservableCollection<string>([FundElements.SingleShareKey]);
-
-            return;
-        }
-
-
-        elements.ShareClassChange(FlowId, Shares.ToArray(), add, remove);
-        //// 同步份额相关的要素 
-
-
-        //if (elements.ShareClasses is null)
-        //    elements.ShareClasses = new(nameof(FundElements.ShareClasses), Shares.Select(x => new ShareClass { Name = x }).ToArray());
-        //else
-        //    elements.ShareClasses.SetValue(Shares.Select(x => new ShareClass { Name = x }).ToArray(), FlowId);
-
-        //elements.Remove()
-
-        db.GetCollection<FundElements>().Update(elements);
-
-        WeakReferenceMessenger.Default.Send(new FundShareChangedMessage { FundId = FundId, FlowId = FlowId });
-    }
-
-
-    /// <summary>
-    /// 如果拆分份额但未保存，则恢复为原始值
-    /// </summary>
-    /// <param name="value"></param>
-    partial void OnIsDividingShareChanged(bool value)
-    {
-        if (!_shareChanged) return; 
-
-        var db = new BaseDatabase();
-        var shareClass = db.GetCollection<FundElements>().FindOne(x => x.FundId == FundId)?.ShareClasses;
-
-        if (shareClass is not null && shareClass.GetValue(FlowId).Value is ShareClass[] shares)
-            Shares = new ObservableCollection<string>(shares.Select(x => x.Name));
-        else
-            Shares = new ObservableCollection<string>([FundElements.SingleShareKey]);
-    }
 
 }
 
