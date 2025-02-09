b6de3ea0a0f49d5bb842b4640643d4ed7c525ccc
iyu <iyumot@163.com>
2025-02-09 17:24:48 +0800

    估值表邮箱

---
+++ FMO.Automation/GatherDailyFromMail/GzMailInfo.cs
@@ -0,0 +1,16 @@
+﻿using LiteDB;
+
+namespace FMO.Schedule;
+
+public class GzMailInfo
+{
+    public required string Id { get; set; }
+
+    public required string Subject { get; set; }
+
+    public DateTime Time { get; set; }
+
+    public GzMailAttachInfo[]? Attachments { get; set; }
+
+    public bool HasError { get; set; }
+}
