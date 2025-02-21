bb0c03cc7f86060d378c74720ab4326c97af2e75
iyumot <iyumot@163.com>
2025-02-21 09:18:10 +0800

    增加合投

---
+++ FMO/Views/Customer/QualificationView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FMO;
+
+/// <summary>
+/// QualificationView.xaml 的交互逻辑
+/// </summary>
+public partial class QualificationView : UserControl
+{
+    public QualificationView()
+    {
+        InitializeComponent();
+    }
+}
+
+
+public partial class QualificationViewModel:ObservableObject
+{
+    [ObservableProperty]
+    public partial bool IsProfessional { get; set; }
+
+}
+
+
