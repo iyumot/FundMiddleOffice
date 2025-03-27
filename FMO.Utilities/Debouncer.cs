4ba54b709adac749b1d84e8eca8e74507752a570
iyumot <iyumot@163.com>
2025-03-27 09:43:39 +0800

    优化debouncer

--- FMO.Utilities/Debouncer.cs
+++ FMO.Utilities/Debouncer.cs
@@ -2,33 +2,36 @@
 {
     private readonly Action _action;
     private readonly int _dueTime;
-    private Timer _timer;
+    private Timer? _timer;
     private Lock _lock = new();
 
-    public Debouncer(Action action, int milseconds)
+    public Debouncer(Action action, int milseconds = 200)
     {
         _action = action ?? throw new ArgumentNullException(nameof(action));
         _dueTime = milseconds > 0 ? milseconds : throw new ArgumentOutOfRangeException(nameof(milseconds));
-        _timer = new Timer(_ => Work(), null, _dueTime, Timeout.Infinite);
+        //_timer = new Timer(_ => Work(), null, Timeout.Infinite, _dueTime);
     }
 
     public void Invoke()
     {
-        if (_timer is null)
-            _timer = new Timer(_ => Work(), null, _dueTime, Timeout.Infinite);
-        else
-            _timer.Change(_dueTime, Timeout.Infinite);
+        lock (_lock)
+        {
+            if (_timer is null)
+                _timer = new Timer(_ => Work(), null, _dueTime, Timeout.Infinite);
+            else
+                _timer.Change(_dueTime, Timeout.Infinite);
+        }
     }
 
     private void Work()
     {
-        _action();
-        _timer.Change(Timeout.Infinite, 0);
+        lock(_lock)
+        {
+            _timer?.Change(Timeout.Infinite, Timeout.Infinite);
+            _action();
+            _timer?.Dispose();
+            _timer = null;
+        }
     }
 
-
-    public void Dispose()
-    {
-        _timer?.Dispose();
-    }
 }
