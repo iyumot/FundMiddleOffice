71ef9b7d05aa357a5cfc4c81593ee4991ea9f0ae
iyumot <iyumot@163.com>
2025-04-18 15:37:40 +0800

    首页

--- FMO/Views/HomePage.xaml.cs
+++ FMO/Views/HomePage.xaml.cs
@@ -1,9 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using FMO.IO.AMAC;
 using FMO.Models;
 using FMO.Schedule;
 using FMO.Utilities;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using Serilog;
 using System.IO;
 using System.Net.Http;
@@ -21,17 +25,11 @@ public partial class HomePage : UserControl
         InitializeComponent();
     }
 
-
-
-
-
-
-
-
-
-
-
-
+    private void UserControl_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        if (e.Key == System.Windows.Input.Key.F5 && DataContext is HomePageViewModel vm)
+            Task.Run(() => vm.InitPlot());
+    }
 }
 
 
@@ -51,6 +49,12 @@ public partial class HomePageViewModel : ObservableObject
     [ObservableProperty]
     public partial bool IsSelfTesting { get; set; }
 
+    [ObservableProperty]
+    public partial PlotModel? FundCountPlot { get; set; }
+
+    [ObservableProperty]
+    public partial PlotModel? FundScalePlot { get; set; }
+
     public HomePageViewModel()
     {
 
@@ -62,6 +66,7 @@ public partial class HomePageViewModel : ObservableObject
 
             await DataSelfTest();
 
+            InitPlot();
 
 
         });
@@ -103,7 +108,7 @@ public partial class HomePageViewModel : ObservableObject
                     int finished = i * 50 + set.Length;
                     if (finished < ned.Length)
                         HandyControl.Controls.Growl.Success($"已同步{finished}个基金，剩余{ned.Length - finished}个");
-                    
+
                     db = DbHelper.Base();
                     db.GetCollection<Fund>().Update(set);
                     db.Dispose();
@@ -154,5 +159,142 @@ public partial class HomePageViewModel : ObservableObject
         IsSelfTesting = false;
     }
 
+
+    public void InitPlot()
+    {
+        using var db = DbHelper.Base();
+        var status = db.GetCollection<Fund>().FindAll().Select(x => x.Status).ToList();
+
+        FundCountPlot = new PlotModel();
+
+        // 创建 PieSeries
+        var pieSeries = new PieSeries
+        {
+            Stroke = OxyColors.Transparent,
+            StrokeThickness = 2.0,
+            InsideLabelPosition = 0.8,
+            AngleSpan = 360,
+            StartAngle = 0
+        };
+
+        // 添加数据点
+        var cnt = status.Count(x => x < FundStatus.Setup);
+        if (cnt > 0)
+            pieSeries.Slices.Add(new PieSlice("发行中", cnt));
+
+        cnt = status.Count(x => x >= FundStatus.Setup && x <= FundStatus.Registration);
+        if (cnt > 0)
+            pieSeries.Slices.Add(new PieSlice("成立备案", cnt));
+
+        cnt = status.Count(x => x == FundStatus.Normal);
+        pieSeries.Slices.Add(new PieSlice($"运行中 {cnt}只", cnt) { Fill = OxyColors.Pink, IsExploded = true });
+        cnt = status.Count(x => x >= FundStatus.StartLiquidation);
+        pieSeries.Slices.Add(new PieSlice($"已清算 {cnt}只", cnt) { Fill = OxyColors.Orange, IsExploded = true });
+
+        // 将 PieSeries 添加到 PlotModel
+        FundCountPlot.Series.Add(pieSeries);
+
+
+        // 计算管理规模
+        var fids = db.GetCollection<Fund>().FindAll().Select(x => x.Id).ToList();
+        if (fids.Count > 0)
+        {
+            var sd = fids.Select(x => db.GetDailyCollection(x).FindAll().Where(x => x is not null).OrderBy(x => x.Date).ToList()).Where(x => x.Count > 0);
+            var mindate = sd.Select(x => x[0].Date).Min(); var maxdate = sd.Select(x => x[0].Date).Max();
+            var tmpdate = mindate;
+            var dates = new List<DateOnly>();
+            dates.Add(mindate);
+            while (tmpdate < maxdate)
+            {
+                tmpdate = tmpdate.AddDays(1);
+                dates.Add(tmpdate);
+            }
+            var scale = new double[dates.Count];
+
+            foreach (var f in sd)
+            {
+                // 对齐第一个
+                var first = dates.IndexOf(f[0].Date);
+                for (int i = 0, j = first; i < f.Count; i++)
+                {
+                    for (; j < dates.Count; j++)
+                    {
+                        if (f[i].Date == dates[j])
+                        {
+                            scale[j++] += (double)f[i].NetAsset;
+                            break;
+                        }
+                    }
+
+                }
+            }
+
+
+            var lineSeries = new LineSeries
+            {
+                Title = "数据系列",
+                StrokeThickness = 2,
+                Color = OxyColors.Blue
+            };
+
+            // 计算月内最大规模
+            var lastmon = dates[0];
+            var max = 0;
+            List<DateOnly> mons = [dates[0]];
+            List<double> sc = [scale[0]];
+            for (int i = 1; i < dates.Count; i++)
+            {
+                if (scale[i] <= 0) continue;
+
+                if ((dates[i].Year > mons[^1].Year || dates[i].Month > mons[^1].Month))
+                {
+                    mons.Add(dates[i]);
+                    sc.Add(scale[i]);
+                }
+                else if (scale[i] > sc[^1])
+                {
+                    sc[^1] = scale[i];
+                }
+
+            }
+
+            for (int i = 0; i < mons.Count; i++)
+            {
+                lineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(new DateTime(mons[i],default)), sc[i] / 10000));
+            }
+            // 创建日期坐标轴
+            var dateAxis = new DateTimeAxis
+            {
+                Position = AxisPosition.Bottom,
+                Title = "日期",
+                StringFormat = "yyyy-MM-dd", 
+            };
+
+
+            FundScalePlot = new PlotModel();
+            FundScalePlot.Series.Add(lineSeries);
+            // 将日期坐标轴添加到 PlotModel
+            FundScalePlot.Axes.Add(dateAxis);
+        }
+
+
+    }
+
+
+    [RelayCommand]
+    public void CalcFee()
+    {
+        try
+        {
+            var di = new DirectoryInfo(System.Diagnostics.Process.GetCurrentProcess().MainModule!.FileName).Parent!;
+
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo { FileName = Path.Combine(di.FullName, "FMO.FeeCalc.exe"), WorkingDirectory = Directory.GetCurrentDirectory() });
+        }
+        catch (Exception e)
+        {
+
+            HandyControl.Controls.Growl.Error($"无法启动计算器，{e.Message}");
+        }
+    }
 
 }
