f3cde0f33d3c03ccacf02b37d7f6e4d71ec77d29
iyumot <iyumot@163.com>
2025-04-22 10:07:05 +0800

    优化要素

--- FMO.Models/Fund/Elements/TemporarilyOpenInfo.cs
+++ FMO.Models/Fund/Elements/TemporarilyOpenInfo.cs
@@ -7,6 +7,11 @@ public class TemporarilyOpenInfo
 {
     public bool IsAllowed { get; set; }
 
+    /// <summary>
+    /// 仅在合同变更、法规
+    /// </summary>
+    public bool IsLimited { get; set; }
+
     public bool AllowPurchase { get; set; }
 
     public bool AllowRedemption { get; set; }
---
+++ FMO.TPLTests/WordTplTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FMO.TPL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMO.TPL.Tests
+{
+    [TestClass()]
+    public class WordTplTests
+    {
+        [TestMethod()]
+        public void GenerateRegisterAnounceTest()
+        {
+            Directory.SetCurrentDirectory("E:\\funds");
+
+            WordTpl.GenerateRegisterAnounce(new Models.Fund { Name = "ffff", Code = "abcdes" }, "承诺.docx");
+             
+        }
+    }
+}
