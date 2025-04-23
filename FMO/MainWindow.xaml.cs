a7902f8092d4f9b06fdab76d96cc5dc7812b54e7
k1k <k1k@seesunfund.com>
2025-04-23 19:49:07 +0800

    调整首页样式

--- FMO/MainWindow.xaml.cs
+++ FMO/MainWindow.xaml.cs
@@ -1,16 +1,16 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using CommunityToolkit.Mvvm.Messaging;
-using FMO.Models;
-using FMO.Utilities;
-using Serilog;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using FMO.Models;
+using FMO.Utilities;
+using Serilog;
 
 namespace FMO;
 
@@ -46,6 +46,22 @@ public partial class MainWindow : HandyControl.Controls.Window
 }
 
 
+public partial class TabItemInfo:ObservableObject
+{
+    public required string Header { get; set; }
+
+    public Brush? Background { get; set; }
+
+    public Brush? HeaderBrush { get; set; } = Brushes.Black;
+
+    public FrameworkElement? Content { get; set; }
+
+    public bool IsCloseable { get; set; } = true;
+
+    [ObservableProperty]
+    public partial bool IsSelected { get; set; }
+
+}
 
 public partial class MainWindowViewModel : ObservableRecipient, IRecipient<string>, IRecipient<OpenFundMessage>
 {
@@ -61,12 +77,12 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
 
 
     [ObservableProperty]
-    public partial ObservableCollection<TabItem> Pages { get; private set; }
+    public partial ObservableCollection<TabItemInfo> Pages { get; private set; }
 
     public MainWindowViewModel()
     {
         IsActive = true;
-        Pages = new ObservableCollection<TabItem>([GenerateHomePageTab()]);
+        Pages = new ObservableCollection<TabItemInfo>([new TabItemInfo { Header = "首页", IsCloseable = false, Content = new HomePage() }]);
     }
 
     public void Receive(string message)
@@ -103,41 +119,14 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
         if (page is null)
         {
             var obj = new FundInfoPage() { Tag = fund.Name, DataContext = new FundInfoPageViewModel(fund, ele) };
-            page = new TabItem { Header = GenerateHeader(fund.ShortName ?? fund.Name ?? "Fund"), Content = obj };
+            page = new TabItemInfo { Header = fund.ShortName ?? fund.Name ?? "Fund", Content = obj, };//new TabItem { Header = GenerateHeader(fund.ShortName ?? fund.Name ?? "Fund"), Content = obj };
             Pages.Add(page);
         }
+
         page.IsSelected = true;
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    private TabItem GenerateHomePageTab()
-    {
-        var ti = new TabItem();
-        var page = new HomePage();
-        ti.Header = new HomePageHeader();
-        ti.Content = page;
-        ti.DataContext = new HomePageViewModel();
-        return ti;
-    }
-
-    private DockPanel GenerateHeader(string head)
-    {
-        var dock = new DockPanel();
-        var btn = new Button { Background = Brushes.Transparent };
-        btn.SetBinding(Button.CommandProperty, "ClosePageCommand");
-        btn.SetBinding(Button.CommandParameterProperty, new Binding { RelativeSource = new RelativeSource { AncestorType = typeof(TabItem) } });
-        btn.Style = App.Current.Resources["ButtonDefault.Small"] as Style;
-        btn.BorderThickness = new Thickness(0);
-        HandyControl.Controls.IconElement.SetGeometry(btn, App.Current.Resources["CloseGeometry"] as Geometry);
-        DockPanel.SetDock(btn, Dock.Right);
-        dock.Children.Add(btn);
-        dock.Children.Add(new TextBlock { Text = head, VerticalAlignment = VerticalAlignment.Center });
-        return dock;
-    }
-
+ 
     [RelayCommand]
     public void OpenPage(string id)
     {
@@ -148,7 +137,7 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
                     var page = Pages.FirstOrDefault(x => x.Content is PlatformPage);
                     if (page is null)
                     {
-                        page = new TabItem { Header = GenerateHeader("外部平台"), Background = Brushes.MediumSpringGreen, Content = new PlatformPage() };
+                        page = new TabItemInfo { Header = "外部平台", Background = Brushes.MediumSpringGreen, Content = new PlatformPage() };
                         Pages.Add(page);
                     }
 
@@ -161,7 +150,7 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
                     var page = Pages.FirstOrDefault(x => x.Content is FundsPage);
                     if (page is null)
                     {
-                        page = new TabItem { Header = GenerateHeader("基金总览"), Background = Brushes.Violet, Content = new FundsPage() };
+                        page = new TabItemInfo { Header = "基金总览", Background = Brushes.Violet, Content = new FundsPage() };
                         Pages.Add(page);
                     }
 
@@ -174,7 +163,7 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
                     var page = Pages.FirstOrDefault(x => x.Content is ManagerPage);
                     if (page is null)
                     {
-                        page = new TabItem { Header = GenerateHeader("管理人"), Background = Brushes.BlueViolet, Foreground = Brushes.White, Content = new ManagerPage() { Foreground = Brushes.Black } };
+                        page = new TabItemInfo { Header = "管理人", HeaderBrush = Brushes.White, Background = Brushes.BlueViolet, Content = new ManagerPage() };// new TabItem { Header = GenerateHeader("管理人"), Background = Brushes.BlueViolet, Foreground = Brushes.White, Content = new ManagerPage() { Foreground = Brushes.Black } };
                         Pages.Add(page);
                     }
 
@@ -186,7 +175,7 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
                     var page = Pages.FirstOrDefault(x => x.Content is TaskPage);
                     if (page is null)
                     {
-                        page = new TabItem { Header = GenerateHeader("任务"), Background = Brushes.Khaki, Content = new TaskPage() };
+                        page = new TabItemInfo { Header =  "任务", Background = Brushes.Khaki, Content = new TaskPage() };
                         Pages.Add(page);
                     }
 
@@ -199,7 +188,7 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
                     var page = Pages.FirstOrDefault(x => x.Content is StatementPage);
                     if (page is null)
                     {
-                        page = new TabItem { Header = GenerateHeader("报表"), Background = new LinearGradientBrush([new GradientStop(Colors.RoyalBlue, 0), new GradientStop(Colors.White, 0.3), new GradientStop(Colors.RoyalBlue, 0.7)]), Content = new StatementPage() };
+                        page = new TabItemInfo { Header =  "报表", Background =  Brushes.RoyalBlue, HeaderBrush=Brushes.White, Content = new StatementPage() };
                         Pages.Add(page);
                     }
 
@@ -212,9 +201,9 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
                     var page = Pages.FirstOrDefault(x => x.Content is CustomerPage);
                     if (page is null)
                     {
-                        page = new TabItem
+                        page = new TabItemInfo
                         {
-                            Header = GenerateHeader("投资人"), 
+                            Header =  ("投资人"),
                             Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#49bc69")),
                             Content = new CustomerPage()
                         };
@@ -230,45 +219,46 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
                     var page = Pages.FirstOrDefault(x => x.Content is TransferRecordPage);
                     if (page is null)
                     {
-                        page = new TabItem { Header = GenerateHeader("  TA  "), Background = Brushes.Orange, Content = new TransferRecordPage() };
+                        page = new TabItemInfo { Header = "  TA  ", Background = Brushes.Orange, Content = new TransferRecordPage() };
                         Pages.Add(page);
                     }
 
                     page.IsSelected = true;
                     break;
                 }
- 
+
 
             default:
-                {
-                    Type? type = Type.GetType($"FMO.{id}");
-                    if (type is null) break;
+                //{
+                //    Type? type = Type.GetType($"FMO.{id}");
+                //    if (type is null) break;
 
-                    var page = Pages.FirstOrDefault(x => x.Content?.GetType() == type);
-                    if (page is null)
-                    {
-                        var obj = Activator.CreateInstance(type) as UserControl;
-                        page = new TabItem { Header = GenerateHeader(obj?.Tag as string ?? "新标签"), Content = obj };
-                        Pages.Add(page);
-                    }
+                //    var page = Pages.FirstOrDefault(x => x.Content?.GetType() == type);
+                //    if (page is null)
+                //    {
+                //        var obj = Activator.CreateInstance(type) as UserControl;
+                //        page = new TabItem { Header = GenerateHeader(obj?.Tag as string ?? "新标签"), Content = obj };
+                //        Pages.Add(page);
+                //    }
 
-                    page.IsSelected = true;
-                }
+                //    page.IsSelected = true;
+                //}
                 break;
         }
 
     }
 
     [RelayCommand]
-    public void ClosePage(TabItem tabItem)
+    public void ClosePage(TabItemInfo tabItem)
     {
         if (tabItem is not null)
         {
             if (tabItem.Content is FrameworkElement e && e.DataContext is not null)
                 e.DataContext = null;
 
+            if (tabItem.Content is not null)
+                tabItem.Content.DataContext = null;
             tabItem.Content = null;
-            tabItem.DataContext = null;
             Pages.Remove(tabItem);
 
         }
