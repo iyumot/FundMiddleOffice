dd256c4b26a893613b9789ee53d0f24914cbfb89
iyu <iyumot@163.com>
2025-04-27 18:37:22 +0800

    开放日

---
+++ FMO.Models/Month.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace FMO;
+
+public enum Month
+{
+    [Description("一月")]
+    January = 1,
+    [Description("二月")]
+    February,
+    [Description("三月")]
+    March,
+    [Description("四月")]
+    April,
+    [Description("五月")]
+    May,
+    [Description("六月")]
+    June,
+    [Description("七月")]
+    July,
+    [Description("八月")]
+    August,
+    [Description("九月")]
+    September,
+    [Description("十月")]
+    October,
+    [Description("十一月")]
+    November,
+    [Description("十二月")]
+    December
+}
