1e65080076a03cab559de0eac5f388f539af209a
k1k <k1k@seesunfund.com>
2025-04-30 17:45:25 +0800

    建投插件

---
+++ FMO.IO.Trustee.CSC108/CSCAssist.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.RegularExpressions;
+using FMO.Models;
+using Microsoft.Playwright;
+using Serilog;
+
+namespace FMO.IO.Trustee;
+
+public class CSCAssist : TrusteeAssistBase, IExternPlatform
+{
+    public override string Identifier => "csc108";
+
+    public override string Name => "中信建投证券股份有限公司";
+
+    public override string Domain => "https://tgfw.csc108.com/";
+
+    public override Regex HoldingCheck { get; init; } = new Regex("csc108.com");
+
+    public override Task<(string Code, ManageFeeDetail[] Fee)[]> GetManageFeeDetails(DateOnly start, DateOnly end)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<bool> LoginValidationOverrideAsync(IPage page)
+    {
+        return await page.GetByText("首页").CountAsync() > 0;
+    }
+
+    public override Task<bool> SynchronizeCustomerAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<bool> SynchronizeDistributionAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<bool> SynchronizeFundRaisingRecord()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<bool> SynchronizeTransferRecordAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<bool> SynchronizeTransferRequestAsync()
+    {
+        // 判断登陆状态
+        if (!IsLogedIn && !await ((IExternPlatform)this).LoginAsync())
+            return false;
+
+        var func_name = nameof(SynchronizeCustomerAsync);
+
+        // 获取页面
+        await using var page = await Automation.AcquirePage(Identifier);
+        if (page.IsNew) await page.GotoAsync(Domain);
+
+        // 获取
+        var locator = page.Locator("#innerSession").GetByText("份额登记");
+        locator = await locator.SingleVisible();
+        if(locator is null)
+        {
+            Log.Error($"{func_name} <份额登记> 不可见");
+            return false;
+        }
+        await locator.ClickAsync();
+
+        // 验证是否弹出
+
+
+
+
+
+
+        return true;
+    }
+}
---
+++ FMO.Interop/FMO.IO.Trustee/LocatorHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+
+namespace FMO.IO.Trustee;
+
+public static class LocatorHelper
+{
+    /// <summary>
+    /// 返回唯一可见项，多个返回null
+    /// </summary>
+    /// <param name="locator"></param>
+    /// <returns></returns>
+    public static async Task<ILocator?> SingleVisible(this ILocator locator)
+    {
+        ILocator? l = null;
+        foreach (var item in await locator.AllAsync())
+        {
+            if (!await item.IsVisibleAsync())
+                continue;
+
+            if (l is not null) return null;
+            l = item;
+        }
+        return l;
+    }
+}
