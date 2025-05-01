7f8a3a2318782e0eb8cb697e8c433e060de5cdb9
iyu <iyumot@163.com>
2025-05-01 09:14:39 +0800

    test

---
+++ FMO.ModelsTests/TradingDayTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMO.Models.Tests
+{
+    [TestClass()]
+    public class TradingDayTests
+    {
+        [TestMethod()]
+        public void GatherTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
