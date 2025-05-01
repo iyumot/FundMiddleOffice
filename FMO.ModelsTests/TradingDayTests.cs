cf3ef137868bc245d0ff40d277765360bff9306e
iyu <iyumot@163.com>
2025-05-01 13:01:03 +0800

    调整交易日实现

--- FMO.ModelsTests/TradingDayTests.cs
+++ FMO.ModelsTests/TradingDayTests.cs
@@ -1,20 +1,70 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FMO.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace FMO.Models.Tests;
 
-namespace FMO.Models.Tests
+
+
+[TestClass()]
+public class TradingDayTests
 {
-    [TestClass()]
-    public class TradingDayTests
+    [TestMethod()]
+    public void GatherTest()
     {
-        [TestMethod()]
-        public void GatherTest()
+        for (int i = 0; i < 100; i++)
         {
-            Assert.Fail();
+
+            var start = DateOnly.FromDayNumber(Random.Shared.Next(1000, 10000));
+            var end = DateOnly.FromDayNumber(Random.Shared.Next(start.DayNumber, 10000));
+
+            int old = TradingDay.CountBetween(start, end);
+
+            int nd = Days.CountTradingDays(start, end);
+
+            Assert.AreEqual(old, nd);
         }
+
+
+
     }
+
+
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var path = @"G:\周报\tt.csv";
+
+
+        List<DateOnly> dates = new List<DateOnly>(20000);
+        List<DayFlag> flags = new List<DayFlag>(20000);
+
+        using var sr = new StreamReader(path);
+        sr.ReadLine();
+        while (!sr.EndOfStream)
+        {
+            var str = sr.ReadLine().Split(',');
+            if (str[1].Length < 4) continue;
+
+            var date = DateOnly.Parse(str[1]);
+
+            var flag = str[3] switch { "非交易日" => DayFlag.Holiday, _ => DayFlag.Trade };
+            if (date.DayOfWeek switch { DayOfWeek.Sunday or DayOfWeek.Saturday => true, _ => false } && flags[^1] != DayFlag.Holiday)
+                flag = DayFlag.Weekend;
+
+
+            dates.Add(date);
+            flags.Add(flag);
+        }
+
+
+        using (var fs = new StreamWriter("test.csv"))
+        {
+            for (int i = 0; i < dates.Count; i++)
+            {
+                if (dates[i].Year > 1991)
+                    fs.WriteLine($"{dates[i]},{flags[i]}");
+            }
+            fs.Flush();
+        }
+
+    }
+
 }
