43f19b74aef0b6f1db7d8bbfba5ea860c9be68ac
k1k <k1k@seesunfund.com>
2025-05-06 19:08:25 +0800

    ta

--- FMO/ToolWindow/AddTAWindow.xaml.cs
+++ FMO/ToolWindow/AddTAWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using FMO.Models;
 using FMO.Utilities;
 
@@ -16,9 +18,12 @@ public partial class AddTAWindow : Window
     public AddTAWindow()
     {
         InitializeComponent();
+
+
+        Loaded += (s, e) => { if (DataContext is AddTAWindowViewModel d) d.SearchInvestorKey = null; };
     }
 
-    private void ComboBox_KeyDown(object sender, KeyEventArgs e)
+    private void Search_KeyDown(object sender, KeyEventArgs e)
     {
         if (sender is ComboBox obj) obj.IsDropDownOpen = true;
     }
@@ -41,6 +46,7 @@ public partial class AddTAWindowViewModel : ObservableObject
 
 
     [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
     public partial Fund? SelectedFund { get; set; }
 
 
@@ -50,10 +56,12 @@ public partial class AddTAWindowViewModel : ObservableObject
     public CollectionViewSource InvestorSource { get; } = new();
 
     [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
     public partial string? SearchInvestorKey { get; set; }
 
 
     [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
     public partial Investor? SelectedInvestor { get; set; }
 
 
@@ -61,10 +69,52 @@ public partial class AddTAWindowViewModel : ObservableObject
 
 
 
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(ShowRequestNumber))]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
+    public partial TARecordType? SelectedType { get; set; }
 
 
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
+    public partial DateTime? RequestDate { get; set; }
 
 
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
+    public partial DateTime? ConfirmDate { get; set; }
+
+
+    public bool ShowRequestNumber => SelectedType switch { TARecordType.Purchase or TARecordType.Subscription or TARecordType.Redemption or TARecordType.ForceRedemption => true, _ => false };
+
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
+    public partial decimal? RequestNumber { get; set; }
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
+    public partial decimal? ConfirmShare { get; set; }
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
+    public partial decimal? ConfirmAmount { get; set; }
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
+    public partial decimal? Fee { get; set; }
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
+    public partial decimal? ConfirmNetAmount { get; set; }
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ConfirmCommand))]
+    public partial decimal? PerformanceFee { get; set; }
+
+
+    [ObservableProperty]
+    public partial string? Tips { get; set; }
 
     public AddTAWindowViewModel()
     {
@@ -94,12 +144,78 @@ public partial class AddTAWindowViewModel : ObservableObject
 
 
 
+    public bool CanConfirm => SelectedFund is not null && SelectedType is not null && SelectedInvestor is not null && RequestDate is not null && ConfirmDate is not null && ConfirmShare is not null && ConfirmAmount is not null && ConfirmNetAmount is not null && SelectedType switch { TARecordType.Distribution or TARecordType.Redemption or TARecordType.ForceRedemption => PerformanceFee is not null, _ => true };
 
 
 
 
 
 
+    [RelayCommand(CanExecute = nameof(CanConfirm))]
+    public void Confirm()
+    {
+        using var db = DbHelper.Base();
+
+        var tq = new TransferRequest
+        {
+            FundId = SelectedFund!.Id,
+            FundCode = SelectedFund!.Code,
+            FundName = SelectedFund!.Name,
+            CustomerId = SelectedInvestor!.Id,
+            CustomerIdentity = SelectedInvestor.Identity.Id,
+            CustomerName = SelectedInvestor!.Name,
+            RequestDate = DateOnly.FromDateTime(RequestDate!.Value),
+            RequestAmount = SelectedType switch { TARecordType.Subscription or TARecordType.Purchase => RequestNumber ?? 0, _ => 0 },
+            RequestShare = SelectedType switch { TARecordType.Redemption or TARecordType.ForceRedemption => RequestNumber ?? 0, _ => 0 },
+            RequestType = SelectedType switch { TARecordType.Subscription => RequestType.Subscription, TARecordType.Purchase => RequestType.Purchase, TARecordType.Redemption => RequestType.Redemption, TARecordType.ForceRedemption => RequestType.ForceRedemption },
+            CreateDate = DateOnly.FromDateTime(DateTime.Today),
+            Source = "manual"
+        };
+        db.GetCollection<TransferRecord>().Insert(tq);
 
 
+        var ta = new TransferRecord
+        {
+            FundId = SelectedFund!.Id,
+            FundCode = SelectedFund!.Code,
+            FundName = SelectedFund!.Name,
+            CustomerId = SelectedInvestor!.Id,
+            CustomerIdentity = SelectedInvestor.Identity.Id,
+            CustomerName = SelectedInvestor!.Name,
+            RequestDate = DateOnly.FromDateTime(RequestDate!.Value),
+            ConfirmedDate = DateOnly.FromDateTime(ConfirmDate!.Value),
+            RequestAmount = SelectedType switch { TARecordType.Subscription or TARecordType.Purchase => RequestNumber ?? 0, _ => 0 },
+            RequestShare = SelectedType switch { TARecordType.Redemption or TARecordType.ForceRedemption => RequestNumber ?? 0, _ => 0 },
+            ConfirmedShare = ConfirmShare ?? 0,
+            ConfirmedAmount = ConfirmAmount ?? 0,
+            ConfirmedNetAmount = ConfirmNetAmount ?? 0,
+            CreateDate = DateOnly.FromDateTime(DateTime.Today),
+            Type = SelectedType!.Value,
+            Source = "manual",
+            Fee = Fee ?? 0,
+            PerformanceFee = PerformanceFee ?? 0,
+            RequestId = tq.Id
+        };
+        db.GetCollection<TransferRecord>().Insert(ta);
+
+        App.Current.Windows[^1].Close();
+    }
+
+    protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        base.OnPropertyChanged(e);
+
+        //if (e.PropertyName == nameof(CanConfirm))
+        if (CanConfirm)
+        {
+            // 检查是否有重复数据
+            using var db = DbHelper.Base();
+            var old = db.GetCollection<TransferRecord>().Find(x => x.FundId == SelectedFund!.Id && x.CustomerId == SelectedInvestor!.Id && x.Type == SelectedType && x.ConfirmedDate == DateOnly.FromDateTime(ConfirmDate!.Value)).ToArray();
+            old = old.Where(x => x.ConfirmedShare == ConfirmShare && x.ConfirmedAmount == ConfirmAmount && x.ConfirmedNetAmount == ConfirmNetAmount && x.PerformanceFee == (PerformanceFee ?? 0)).ToArray();
+
+            if (old.Any())
+                Tips = $"可能存在重复数据";
+            else Tips = null;
+        }
+    }
 }
