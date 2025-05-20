b07c0f2a87c045c0cb721ee4b927dd380ef8578b
k1k <k1k@seesunfund.com>
2025-05-20 16:40:51 +0800

    test

--- FMO.PDFTests/PdfHelperTests.cs
+++ FMO.PDFTests/PdfHelperTests.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FMO.PDF;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PDFiumSharp;
 
 namespace FMO.PDF.Tests
 {
@@ -14,7 +8,7 @@ namespace FMO.PDF.Tests
         [TestMethod()]
         public void GetSecurityAccountsTest()
         {
-            using var fs = new FileStream(@"C:\Users\lenovo\Downloads\5555\鑫享世宸量化选股1号私募证券投资基金变更回执.pdf", FileMode.Open);
+            using var fs = new FileStream(@"C:\Users\lenovo\Downloads\5555\a.pdf", FileMode.Open);
 
             var res = PdfHelper.GetSecurityAccounts(fs);
 
@@ -25,7 +19,7 @@ namespace FMO.PDF.Tests
         [TestMethod]
         public void GetAccountInfo()
         {
-            using var fs = new FileStream(@"C:\Users\lenovo\Downloads\鑫享世宸量化CTA1号A期私募证券投资基金_募集账户信息函.pdf", FileMode.Open);
+            using var fs = new FileStream(@"C:\Users\lenovo\Downloads\b.pdf", FileMode.Open);
             PdfHelper.GetAccountInfo(fs);
         }
 
@@ -34,6 +28,29 @@ namespace FMO.PDF.Tests
         {
             var d = new DirectoryInfo("-");
             d.Create();
+        }
+
+
+        [TestMethod]
+        public void TaPdf()
+        {
+            var d = PDFium.FPDF_LoadDocument(@"C:\Users\lenovo\Downloads\x.pdf", null);
+            var cnt = PDFium.FPDF_GetPageCount(d);
+            if (cnt == 0) return;
+
+            var page = PDFium.FPDF_LoadPage(d, 0);
+            var textpage = PDFium.FPDFText_LoadPage(page);
+            var charcnt = PDFium.FPDFText_CountChars(textpage);
+            if (charcnt == 0) return;
+
+            var rotate = PDFium.FPDFPage_GetRotation(page);
+
+            var str = PDFium.FPDFText_GetText(textpage, 0, charcnt);
+             
+
+            PDFium.FPDFText_ClosePage(textpage);
+            PDFium.FPDF_ClosePage(page);
+
         }
     }
 }
---
+++ FMO.ScheduleTests/MissionTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FMO.Schedule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDataReader;
+using System.IO;
+using System.Diagnostics;
+
+namespace FMO.Schedule.Tests
+{
+    [TestClass()]
+    public class MissionTests
+    {
+        [TestMethod()]
+        public void TaTest()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using var stream = new FileStream(@"C:\Users\lenovo\Downloads\历史交易确认明细(含当日已确认)_20250520_97b59d10256e42e38423939e9b855130.xlsx", FileMode.Open);
+
+
+            if (stream is null || stream.Length == 0) return;
+
+            if (stream.CanSeek)
+                stream.Seek(0, SeekOrigin.Begin);
+
+            var reader = ExcelReaderFactory.CreateReader(stream);
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                reader.Read();
+                for (int j = 0; j < reader.FieldCount; j++)
+                {
+                    var d = reader.GetValue(j);
+
+                    Debug.Write($"{d},");
+                    
+                }
+                Debug.WriteLine("");
+            }
+
+
+
+
+
+        }
+    }
+}
