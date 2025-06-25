12f6298f7d53b789fc19b287bb429baa04cb9b13
iyumot <iyumot@163.com>
2025-06-25 23:05:41 +0800

    ui

--- FMO/MainWindow.xaml.cs
+++ FMO/MainWindow.xaml.cs
@@ -91,7 +91,7 @@ public partial class MainWindowViewModel : ObservableRecipient, IRecipient<strin
         MenuItems = [new MainMenu { Title = "管理人", IconBrush = Brushes.BlueViolet, Command = OpenPageCommand, Parameter = "ManagerPage", Icon = GetGeometry("f.house") },
                     new MainMenu { Title = "基金", IconBrush = Brushes.Violet, Command = OpenPageCommand, Parameter = "FundsPage", Icon = GetGeometry("f.fire")},
                     new MainMenu { Title = "客户", IconBrush = Brushes.ForestGreen, Command = OpenPageCommand, Parameter = "Customer", Icon = GetGeometry("f.user")},
-                    new MainMenu { Title = "TA", IconBrush = Brushes.Orange, Command = OpenPageCommand, Parameter = "TA", Icon = GetGeometry("f.table")},
+                    new MainMenu { Title = "TA", IconBrush = Brushes.Orange, Command = OpenPageCommand, Parameter = "TA", Icon = GetGeometry("f.calendar-days")},
                     new MainMenu { Title = "平台", IconBrush = Brushes.MediumSpringGreen, Command = OpenPageCommand, Parameter = "Trustee", Icon = GetGeometry("f.tree")},
                     new MainMenu { Title = "任务", IconBrush = Brushes.DarkOrchid, Command = OpenPageCommand, Parameter = "Task", Icon = GetGeometry("f.brain")},
                     new MainMenu { Title = "报表", IconBrush = Brushes.RoyalBlue, Command = OpenPageCommand, Parameter = "Statement", Icon = GetGeometry("f.square-poll-vertical")},  ];
