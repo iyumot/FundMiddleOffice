ac1e2fdd10f75fafbaba56f6aaf35209d70d4e83
k1k <k1k@seesunfund.com>
2025-07-18 09:11:21 +0800

    增加 api 运行配置修改窗口，未完善

--- FMO/Views/TrusteeWorkerSettingView.xaml.cs
+++ FMO/Views/TrusteeWorkerSettingView.xaml.cs
@@ -40,39 +40,30 @@ public partial class TrusteeWorkerSettingViewModel : ObservableObject
         using var pdb = DbHelper.Platform();
         var ranges = pdb.GetCollection<TrusteeMethodShotRange>().FindAll().ToArray();
 
-        var cms = new TrusteeWorkingConfigViewModel { Identifier = CMS._Identifier, };
-        cms.QueryTransferRecord = Create(ranges, CMS._Identifier, nameof(ITrustee.QueryTransferRecords));
-        cms.QueryTransferRequest = Create(ranges, CMS._Identifier, nameof(ITrustee.QueryTransferRequests));
-        cms.QueryRaisingAccountTransction = Create(ranges, CMS._Identifier, nameof(ITrustee.QueryRaisingAccountTransction));
-        cms.QueryRaisingBalance = Create(ranges, CMS._Identifier, nameof(ITrustee.QueryRaisingBalance));
-        cms.QueryFundDailyFee = Create(ranges, CMS._Identifier, nameof(ITrustee.QueryFundDailyFee));
-
-
-
-        var citics = new TrusteeWorkingConfigViewModel { Identifier = CITICS._Identifier, };
-        citics.QueryTransferRecord = Create(ranges, CITICS._Identifier, nameof(ITrustee.QueryTransferRecords)); 
-        citics.QueryTransferRequest = Create(ranges, CITICS._Identifier, nameof(ITrustee.QueryTransferRequests));
-        citics.QueryRaisingAccountTransction = Create(ranges, CITICS._Identifier, nameof(ITrustee.QueryRaisingAccountTransction));
-        citics.QueryRaisingBalance = Create(ranges, CITICS._Identifier, nameof(ITrustee.QueryRaisingBalance));
-        citics.QueryFundDailyFee = Create(ranges, CITICS._Identifier, nameof(ITrustee.QueryFundDailyFee));
-
-
-        var csc = new TrusteeWorkingConfigViewModel { Identifier = CSC._Identifier, };
-        csc.QueryTransferRecord = Create(ranges, CSC._Identifier, nameof(ITrustee.QueryTransferRecords));
-        csc.QueryTransferRequest = Create(ranges, CSC._Identifier, nameof(ITrustee.QueryTransferRequests));
-        csc.QueryRaisingAccountTransction = Create(ranges, CSC._Identifier, nameof(ITrustee.QueryRaisingAccountTransction));
-        csc.QueryRaisingBalance = Create(ranges, CSC._Identifier, nameof(ITrustee.QueryRaisingBalance));
-        csc.QueryFundDailyFee = Create(ranges, CSC._Identifier, nameof(ITrustee.QueryFundDailyFee));
+        var cms = Create(CMS._Identifier, ranges);
+        var citics = Create(CITICS._Identifier, ranges);
+        var csc = Create(CSC._Identifier, ranges);
 
         Configs = [cms, citics, csc];
     }
 
+    private TrusteeWorkingConfigViewModel Create(string idf, TrusteeMethodShotRange[] ranges)
+    {
+        var citics = new TrusteeWorkingConfigViewModel { Identifier = idf, };
+        citics.QueryTransferRecord = Create(ranges, idf, nameof(ITrustee.QueryTransferRecords));
+        citics.QueryTransferRequest = Create(ranges, idf, nameof(ITrustee.QueryTransferRequests));
+        citics.QueryRaisingAccountTransction = Create(ranges, idf, nameof(ITrustee.QueryRaisingAccountTransction));
+        citics.QueryRaisingBalance = Create(ranges, idf, nameof(ITrustee.QueryRaisingBalance));
+        citics.QueryFundDailyFee = Create(ranges, idf, nameof(ITrustee.QueryFundDailyFee));
+        return citics;
+    }
 
     public TrusteeMethodConfigViewModel Create(TrusteeMethodShotRange[] ranges, string identifier, string method)
     {
         var range = ranges.FirstOrDefault(x => x.Id == identifier + method);
         return new TrusteeMethodConfigViewModel
         {
+            Identifier = identifier,
             Method = method,
             DateBegin = range?.Begin,
             DateEnd = range?.End
@@ -84,6 +75,8 @@ public partial class TrusteeWorkerSettingViewModel : ObservableObject
 
 public partial class TrusteeMethodConfigViewModel : ObservableObject
 {
+    public required string Identifier { get; set; }
+
     public required string Method { get; set; }
 
     [ObservableProperty]
@@ -92,7 +85,20 @@ public partial class TrusteeMethodConfigViewModel : ObservableObject
     [ObservableProperty]
     public partial DateOnly? DateEnd { get; set; }
 
+    [RelayCommand]
+    public void SaveDataRange(DataGrid v)
+    {
+        using var pdb = DbHelper.Platform();
 
+        if (DateBegin is not null && DateEnd is not null)
+        {
+            var ran = new TrusteeMethodShotRange(Identifier + Method, DateBegin.Value, DateEnd.Value);
+
+            var ranges = pdb.GetCollection<TrusteeMethodShotRange>().Upsert(ran);
+        }
+        
+        v.CommitEdit();
+    }
 }
 
 public partial class TrusteeWorkingConfigViewModel : ObservableObject
@@ -106,16 +112,5 @@ public partial class TrusteeWorkingConfigViewModel : ObservableObject
     public TrusteeMethodConfigViewModel? QueryRaisingBalance { get; internal set; }
     public TrusteeMethodConfigViewModel? QueryFundDailyFee { get; internal set; }
 
-    [RelayCommand]
-    public void Save(TrusteeMethodConfigViewModel v)
-    {
-        using var pdb = DbHelper.Platform();
-
-        if (v.DateBegin is not null && v.DateEnd is not null)
-        {
-            var ran = new TrusteeMethodShotRange(Identifier + v.Method, v.DateBegin.Value, v.DateEnd.Value);
-
-            var ranges = pdb.GetCollection<TrusteeMethodShotRange>().Upsert(ran);
-        }
-    }
+ 
 }
