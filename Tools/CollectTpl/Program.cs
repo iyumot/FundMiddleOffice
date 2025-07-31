fa68fe1a51078f21f5f779b25a400b13ad28d26d
iyumot <iyumot@163.com>
2025-07-31 14:10:25 +0800

    fix

--- Tools/CollectTpl/Program.cs
+++ Tools/CollectTpl/Program.cs
@@ -11,7 +11,9 @@ using System.IO.Compression;
 //#endif
 
 var di = new DirectoryInfo(Directory.GetCurrentDirectory());
-while(di.Parent?.Name != "FMO")
+Console.WriteLine($"Start {di.FullName}");
+
+while (di.Parent?.Name != "FMO")
 {
     di = di.Parent ?? throw new DirectoryNotFoundException("Templates directory not found.");
 }
@@ -21,9 +23,14 @@ di = new DirectoryInfo(Path.Combine(di.FullName, @"..\Templates"));
 var folders = di.GetDirectories();
 foreach (var fd in folders)
 {
+    Console.WriteLine($"Work On {fd.FullName}");
+
     var tf = fd.GetFiles("tpl.dll", SearchOption.AllDirectories).FirstOrDefault();
-    if (tf is null) continue;
-     
+    if (tf is null)
+    { 
+        Console.WriteLine($"     未找到tpl.dll");
+        continue;
+    }
     using var fs = new FileStream(Path.Combine(di.FullName.Replace("Templates", "TPL"), $"{fd.Name}.zip"), FileMode.Create);
     using ZipArchive zipArchive = new ZipArchive(fs, ZipArchiveMode.Create); 
     zipArchive.CreateEntryFromFile(tf.FullName, tf.Name, CompressionLevel.Optimal);
@@ -33,7 +40,8 @@ foreach (var fd in folders)
         zipArchive.CreateEntryFromFile(e.FullName, e.Name, CompressionLevel.Optimal); 
     }
 
-     
+
+    Console.WriteLine($"     Success");
 }
 
 
