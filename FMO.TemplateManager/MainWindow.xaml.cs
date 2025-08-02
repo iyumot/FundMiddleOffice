8073dcacc700693a4244b0186738ca30c00e4c02
iyu <iyumot@163.com>
2025-08-02 09:35:36 +0800

    模板路径改成相对

--- FMO.TemplateManager/MainWindow.xaml.cs
+++ FMO.TemplateManager/MainWindow.xaml.cs
@@ -181,7 +181,7 @@ public partial class MainWindowViewModel : ObservableObject
         archive.ExtractToDirectory(di.FullName, true);
 
         using var db = DbHelper.Base();
-        TemplateInfo entity = new(gid, obj.Name, obj.Description, gen.FullName!, obj.Suit, obj.Meta, di.FullName);
+        TemplateInfo entity = new(gid, obj.Name, obj.Description, gen.FullName!, obj.Suit, obj.Meta, Path.GetRelativePath(Directory.GetCurrentDirectory(), di.FullName));
         db.GetCollection<TemplateInfo>().Upsert(entity);
 
         foreach (var item in Templates.Where(x => x.Id == entity.Id).ToArray())
