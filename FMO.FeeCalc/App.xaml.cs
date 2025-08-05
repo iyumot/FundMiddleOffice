214c768f6d61085f213e741d1df0b5e0fa192e9d
k1k <k1k@seesunfund.com>
2025-08-05 09:58:34 +0800

    调整 工作目录

--- FMO.FeeCalc/App.xaml.cs
+++ FMO.FeeCalc/App.xaml.cs
@@ -1,19 +1,28 @@
-﻿using System.IO;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
 
-namespace FMO.FeeCalc
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-        public App()
-        {
-#if DEBUG
-            Directory.SetCurrentDirectory(@"E:\fmo");
-#endif
-        }
-    }
+namespace FMO.FeeCalc;
 
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+#if DEBUG 
+        using var key = Registry.CurrentUser.OpenSubKey(@$"Software\Nexus\Debug");
+        if (key != null)
+        {
+            var workFolder = key.GetValue("WorkingFolder") as string;
+            if (!string.IsNullOrWhiteSpace(workFolder))
+            {
+                var di = new DirectoryInfo(workFolder);
+                if (di.Exists)
+                    Directory.SetCurrentDirectory(di.FullName);
+            }
+        }
+#endif
+    }
 }
