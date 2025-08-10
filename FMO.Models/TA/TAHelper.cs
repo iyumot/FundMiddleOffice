afac9421d003c2b9611b287ea1464cb37fe8195e
iyu <iyumot@163.com>
2025-08-10 16:37:29 +0800

    add order 扩展函数

--- FMO.Models/TA/TAHelper.cs
+++ FMO.Models/TA/TAHelper.cs
@@ -1,4 +1,5 @@
 ﻿using FMO.Models;
+using System.Diagnostics;
 
 namespace FMO.Utilities;
 
@@ -19,7 +20,7 @@ public static class TAHelper
     }
 
     public static bool IsBuy(this TransferRequest r) => r.RequestType.IsBuy();
-    
+
     public static bool IsSell(this TransferRequest r) => r.RequestType.IsSell();
 
     public static bool RequiredOrder(this TransferRequest r) => r.RequestType.RequiredOrder();
@@ -45,4 +46,23 @@ public static class TAHelper
     public static bool IsSell(this TransferRecord r) => r.Type.IsSell();
 
     public static bool RequiredOrder(this TransferRecord r) => r.Type.RequiredOrder();
+
+
+    public static bool IsBuy(this TransferOrderType r) => r switch { TransferOrderType.FirstTrade or TransferOrderType.Buy => true, _ => false };
+
+    public static bool IsSell(this TransferOrderType r) => r switch { TransferOrderType.RemainAmout or TransferOrderType.Share or TransferOrderType.Amount => true, _ => false };
+
+
+    public static bool IsBuy(this TransferOrder r) => r.Type.IsBuy();
+
+    public static bool IsSell(this TransferOrder r) => r.Type.IsSell();
+
+    public static bool IsCompatible(this TransferOrder o, TransferRequest r) => (o.IsBuy() && r.IsBuy()) || (o.IsSell() && r.IsSell());
+    public static bool IsCompatible(this TransferRequest o, TransferOrder r) => (o.IsBuy() && r.IsBuy()) || (o.IsSell() && r.IsSell());
+
+    public static bool IsCompatible(this TransferOrder o, TransferRecord r) => (o.IsBuy() && r.IsBuy()) || (o.IsSell() && r.IsSell());
+    public static bool IsCompatible(this TransferRecord o, TransferOrder r) => (o.IsBuy() && r.IsBuy()) || (o.IsSell() && r.IsSell());
+
+
+
 }
