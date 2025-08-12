880a2eab043974d231363acec53b6d669dbe6d43
k1k <k1k@seesunfund.com>
2025-08-12 14:21:17 +0800

    调整 工作目录

--- DatabaseViewer/App.xaml.cs
+++ DatabaseViewer/App.xaml.cs
@@ -1,14 +1,41 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
 
-namespace DatabaseViewer
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace DatabaseViewer;
 
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+#if RELEASE
+        // 设置工作目录
+        
+        //if (!string.IsNullOrWhiteSpace(Config.Default.WorkFolder))
+        //{
+        //    var di = new DirectoryInfo(Config.Default.WorkFolder);
+        //    if (di.Exists)
+        //        Directory.SetCurrentDirectory(di.FullName);
+        //}
+      // 从注册表读取
+        using (var key = Registry.CurrentUser.OpenSubKey(@$"Software\Nexus"))       
+#else
+        using (var key = Registry.CurrentUser.OpenSubKey(@$"Software\Nexus\Debug"))
+#endif        
+        {
+            if (key != null)
+            {
+                var workFolder = key.GetValue("WorkingFolder") as string;
+                if (!string.IsNullOrWhiteSpace(workFolder))
+                {
+                    var di = new DirectoryInfo(workFolder);
+                    if (di.Exists)
+                        Directory.SetCurrentDirectory(di.FullName);
+                }
+            }
+        }
+    }
 }
