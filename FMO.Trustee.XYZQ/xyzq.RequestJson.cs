89947cffb027eed35e74180d37a46e11ef97254f
k1k <k1k@seesunfund.com>
2025-08-21 11:09:02 +0800

    xyzq

--- FMO.Trustee.XYZQ/xyzq.RequestJson.cs
+++ FMO.Trustee.XYZQ/xyzq.RequestJson.cs
@@ -7,7 +7,7 @@ namespace FMO.Trustee;
 #pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑添加 "required" 修饰符或声明为可为 null。
 internal class RequestJson : JsonBase
 {
-    //public override string? JsonId =>  ;
+    public override string? JsonId => $"{ProductCode}.{TradingAccount}{BusinessType}{ApplicationDate}" ;
 
 
     /// <summary>
@@ -124,7 +124,7 @@ internal class RequestJson : JsonBase
             RequestShare = ParseDecimal(ApplicationShares),
             Agency = Saler,
             FeeDiscount = ParseDecimal(ApplicationDiscount),
-            ExternalId = $"{XYZQ._Identifier}.",
+            ExternalId = $"{XYZQ._Identifier}.{JsonId}",
             Source = "api"
         };
 
