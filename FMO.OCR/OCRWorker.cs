d5b6c72187e80510974c8afc0e8e8bb5a83db648
k1k <k1k@seesunfund.com>
2025-09-03 13:04:50 +0800

    ocr

--- FMO.OCR/OCRWorker.cs
+++ FMO.OCR/OCRWorker.cs
@@ -1,35 +1,27 @@
 ﻿using OpenCvSharp;
 using Sdcb.PaddleInference;
 using Sdcb.PaddleOCR;
-using Sdcb.PaddleOCR.Models;
+using Sdcb.PaddleOCR.Models.Online;
+using System;
 
 namespace FMO.OCR;
 
 public static class OCRWorker
 {
-    public static void Init()
+    public static async Task<PaddleOcrResult> VerifyCode(byte[] buf)
     {
-        FullOcrModel model = FullOcrModel.FromDirectory("", "","","", ModelVersion.V4);// new FullOcrModel(DetectionModel.FromDirectory(""ModelVersion.V4))
+        var model = await OnlineFullModels.EnglishV4.DownloadAsync();
 
-
-
-
-        using (PaddleOcrAll all = new PaddleOcrAll(model, PaddleDevice.Mkldnn())
+        using PaddleOcrAll all = new PaddleOcrAll(model, PaddleDevice.Mkldnn())
         {
             AllowRotateDetection = true, /* 允许识别有角度的文字 */
             Enable180Classification = false, /* 允许识别旋转角度大于90度的文字 */
-        })
+        };
+
+        using (Mat src = Cv2.ImDecode(buf, ImreadModes.Color))
         {
-            // Load local file by following code: 
-            using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
-            {
-                PaddleOcrResult result = all.Run(src);
-                Console.WriteLine("Detected all texts: \n" + result.Text);
-                foreach (PaddleOcrResultRegion region in result.Regions)
-                {
-                    Console.WriteLine($"Text: {region.Text}, Score: {region.Score}, RectCenter: {region.Rect.Center}, RectSize:    {region.Rect.Size}, Angle: {region.Rect.Angle}");
-                }
-            }
+            return all.Run(src);
         }
+
     }
 }
